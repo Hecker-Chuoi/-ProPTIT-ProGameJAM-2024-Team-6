2bc80fdfc5a22e40bd4bef03193cdc5555e85fd3
PhQuangMinh <phamquangminh15012004@gmail.com>
2024-05-13 15:09:08 +0700

    feat, refactor: hoàn thiện animation, refactor get animation, thêm logic rửa bát

--- ProGameCup/core/src/com/mygdx/game/view/music/PlaySound.java
+++ ProGameCup/core/src/com/mygdx/game/view/music/PlaySound.java
@@ -1,14 +1,8 @@
 package com.mygdx.game.view.music;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.SpaceGame;
-import com.mygdx.game.common.constant.GameConstant;
-import com.mygdx.game.view.NewButton;
-import com.mygdx.game.view.screens.MainMenuScreen;
 
 public class PlaySound {
     private static PlaySound instance;
@@ -29,9 +23,9 @@ public class PlaySound {
     }
 
     public void loadMusic() {
-        assetManager.load("Smooth Sailing.mp3", Music.class);
+        assetManager.load("music/SmoothSailing.mp3", Music.class);
         assetManager.finishLoading();
-        music = assetManager.get("Smooth Sailing.mp3", Music.class);
+        music = assetManager.get("music/SmoothSailing.mp3", Music.class);
         music.setLooping(true);
         music.setVolume(0.2f);
         music.play();
@@ -44,25 +38,7 @@ public class PlaySound {
     public void playMusic(){
         music.play();
     }
-//    public void checkMusic(Texture musicOn, Texture musicOff, int x, int y, int width, int height) {
-//        if (Gdx.input.getX() >= x && Gdx.input.getX() <= x + width && GameConstant.windowHeight - Gdx.input.getY() >= y && GameConstant.windowHeight - Gdx.input.getY() <= y + height) {
-////            if(isMusicOn) batch.draw(musicOnPress, x, y ,width, height);
-////            else batch.draw(musicOffPress,x,y,width,height);
-//            if (Gdx.input.isTouched()) {
-//                if (isMusicOn) {
-////                    batch.draw(musicOff,x,y,width,height);
-//                    music.stop();
-//                    isMusicOn = false;
-//                }
-//                else {
-////                    batch.draw(musicOn, x, y, width, height);
-//                    music.play();
-//
-//                    isMusicOn = true;
-//                }
-//            }
-//        }
-//    }
+
 
     public void dispose(){
         music.dispose();
